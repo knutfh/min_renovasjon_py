52d20c9df3f20c939775fc6199019a8f9c644b05
Knut Flage Henriksen <post@flaksen.com>
2020-04-05 10:49:41 +0200

    Blackify

--- minrenovasjon/constants.py
+++ minrenovasjon/constants.py
@@ -1,6 +1,8 @@
-MUNICIPALITY_CODE_URL = 'https://register.geonorge.no/api/sosi-kodelister/kommunenummer.json'
-KOMTEK_API_BASE_URL = 'https://norkartrenovasjon.azurewebsites.net/proxyserver.ashx'
-KOMTEK_API_ENDPOINT_URL = 'https://komteksky.norkart.no/komtek.renovasjonwebapi/api/'
-APP_CUSTOMERS_URL = 'https://www.webatlas.no/wacloud/servicerepository/CatalogueService.svc/json/GetRegisteredAppCustomers'
-APP_KEY = 'AE13DEEC-804F-4615-A74E-B4FAC11F0A30'
-ADDRESS_LOOKUP_URL = 'https://ws.geonorge.no/adresser/v1/sok?'
+MUNICIPALITY_CODE_URL = (
+    "https://register.geonorge.no/api/sosi-kodelister/kommunenummer.json"
+)
+KOMTEK_API_BASE_URL = "https://norkartrenovasjon.azurewebsites.net/proxyserver.ashx"
+KOMTEK_API_ENDPOINT_URL = "https://komteksky.norkart.no/komtek.renovasjonwebapi/api/"
+APP_CUSTOMERS_URL = "https://www.webatlas.no/wacloud/servicerepository/CatalogueService.svc/json/GetRegisteredAppCustomers"
+APP_KEY = "AE13DEEC-804F-4615-A74E-B4FAC11F0A30"
+ADDRESS_LOOKUP_URL = "https://ws.geonorge.no/adresser/v1/sok?"
